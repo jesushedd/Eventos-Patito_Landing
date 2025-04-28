567184158f598260f3d2e0043f2267edb99ef033
jesushedd <jesushe99@gmail.com>
2025-04-28 13:26:19 -0500

    Instalar app eevntos_landing, include urls

--- back/config/settings.py
+++ back/config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'eventos_landing.apps.EventosLandingConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
