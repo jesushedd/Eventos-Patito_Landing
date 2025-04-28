a80ceebb0e0dd77e7206b47fe868352a8cc61715
jesushedd <jesushe99@gmail.com>
2025-04-28 13:24:28 -0500

    Update urls, views Ahora la url raiz muestra el landing page

--- back/eventos_landing/views.py
+++ back/eventos_landing/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Pregunta
+
+def index(request):
+    return render(request, "eventos_landing/index.html", {'preguntas_lista': Pregunta.objects.all()}) 
