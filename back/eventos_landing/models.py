682c5dc1a5c5dd9d19b7fb0c65f0e46f121ccf3e
jesushedd <jesushe99@gmail.com>
2025-04-28 13:22:23 -0500

    Actualizar modelos

--- back/eventos_landing/models.py
+++ back/eventos_landing/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.db.models import UniqueConstraint
+
 
 # Create your models here.
+
+class Pregunta(models.Model):
+    texto = models.CharField(max_length=200)
+    nombre = models.CharField(max_length=50)
+    def __str__(self):
+        return  self.texto
+    
+class Opcion(models.Model):
+    pregunta = models.ForeignKey(Pregunta, on_delete=models.CASCADE)
+    id_texto = models.CharField(max_length=100)
+    def __str__(self):
+        return  self.nombre
+    class Meta:
+        constraints = [
+            UniqueConstraint(fields=['id_texto'], name="Opcion no repetida")
+        ]
+
